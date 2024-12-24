35e60910a7461b105492521f8e964d4e5ddc93c4
Richard M <160991580+richarrd92@users.noreply.github.com>
2024-12-23 19:29:32 -0500

    update backend app

--- backend/vidtube/src/index.js
+++ backend/vidtube/src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello from richard maliyetu - we are live");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./database/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Mongodb connection error ", err);
+  });
